bb1d1cb0ee7a30207044af7a80b52c5848352977
Jack Weng <wejaq@Jacks-MacBook-Pro.local>
2018-02-14 15:13:34 +0100

    server db

--- server/manager.py
+++ server/manager.py
@@ -3,11 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-from server import app, db
+from server import application, db
 
-migrate = Migrate(app, db)
+migrate = Migrate(application, db)
 
-manager = Manager(app)
+manager = Manager(application)
 manager.add_command('migrate', MigrateCommand)
 
 if __name__ == '__main__':
